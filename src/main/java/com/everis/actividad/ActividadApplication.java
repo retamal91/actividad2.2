4725dab00cef0ddf817d1d2c75022532a40dc7a6
sherrera0198 <sherrera0198@gmail.com>
2021-05-03 16:17:16 -0400

    clase persona

--- src/main/java/com/everis/actividad/ActividadApplication.java
+++ src/main/java/com/everis/actividad/ActividadApplication.java
@@ -3,11 +3,19 @@ package com.everis.actividad;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.everis.actividad.util.Validaciones;
+
 @SpringBootApplication
 public class ActividadApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ActividadApplication.class, args);
+		
+		
+		boolean rut = Validaciones.validarRut("19.638.065-5");
+		
+		System.out.println(rut);
 	}
+
 
 }
