faa1e080ebd779c8e621221083c7307dc0c17379
sherrera0198 <sherrera0198@gmail.com>
2021-05-03 16:10:20 -0400

    clase persona

--- src/main/java/com/everis/actividad/models/Persona.java
+++ src/main/java/com/everis/actividad/models/Persona.java
@@ -1,5 +1,80 @@
 package com.everis.actividad.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table ( name = "personas")
 public class Persona {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Size(min=8,max=12 )
+	private String rut;
+	@Size(min=3,max= 20)
+	private String nombre;
+	@Size(min=3,max= 20)
+	private String apellido;
+	@Size(min=10,max= 40)
+	private String email;
+	
+	public Persona() {}
+	
+	public Persona(@Size(min = 8, max = 12) String rut, @Size(min = 3, max = 20) String nombre,
+			@Size(min = 3, max = 20) String apellido, @Size(min = 10, max = 40) String email) {
+		super();
+		this.rut = rut;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRut() {
+		return rut;
+	}
+
+	public void setRut(String rut) {
+		this.rut = rut;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+	
+	
 }
